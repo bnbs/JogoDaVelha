a9c7969ac30645214ba41f3308b820157323bce3
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2019-02-09 13:11:38 -0200

    [ADD] Adicionado HttpConfigInterceptor [ADD] Adicionado service Auth [MAK] Removido momentjs

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpConfigInterceptor } from '../../interceptor/http-config.interceptor';
 import { SelectHeroComponent } from '../select-hero/select-hero.component';
 import { TicTacToeComponent } from '../tic-tac-toe/tic-tac-toe.component';
 import { SearchInputComponent } from '../ui/search-input/search-input.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
    declarations: [
@@ -23,7 +23,9 @@ import { SearchInputComponent } from '../ui/search-input/search-input.component'
       ReactiveFormsModule,
       FormsModule,
    ],
-   providers: [],
+   providers: [
+      { provide: HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true }
+   ],
    bootstrap: [
       AppComponent
    ]
