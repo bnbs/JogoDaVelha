876405d627aecf45b65b9b883e18aa14fc06fb2e
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2019-02-11 17:26:01 -0200

    [ADD] Criaçao de testes para o component SelectHero

--- src/app/components/select-hero/select-hero.component.spec.ts
+++ src/app/components/select-hero/select-hero.component.spec.ts
@@ -4,14 +4,18 @@ import { By } from '@angular/platform-browser';
 import { DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { SelectHeroComponent } from './select-hero.component';
+import { AlertService } from 'src/app/services/alert.service';
 
 describe('SelectHeroComponent', () => {
   let component: SelectHeroComponent;
   let fixture: ComponentFixture<SelectHeroComponent>;
+  let debugElement: DebugElement;
+  let alertService: AlertService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SelectHeroComponent ],
+      providers: [ AlertService ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
@@ -20,10 +24,36 @@ describe('SelectHeroComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectHeroComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
+    alertService = debugElement.injector.get(AlertService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onStartGame()', () => {
+    const startGame = spyOn(component, 'onStartGame');
+    const startButton = debugElement.query(By.css('button'));
+    startButton.triggerEventHandler('click', null);
+    expect(startGame).toHaveBeenCalled();
+  });
+
+  it('should call error alert', () => {
+    const heroError = spyOn(alertService, 'onHeroError');
+    const startButton = debugElement.query(By.css('button'));
+    startButton.triggerEventHandler('click', null);
+    expect(heroError).toHaveBeenCalled();
+  });
+
+  it('should call success alert', () => {
+    component.onSelectedHero({name: 'Hulk', thumbnail: ''}, 1);
+    component.onSelectedHero({name: 'Groot', thumbnail: ''}, 2);
+    const heroSuccess = spyOn(alertService, 'onHeroSuccess');
+    const startButton = debugElement.query(By.css('button'));
+    startButton.triggerEventHandler('click', null);
+    expect(heroSuccess).toHaveBeenCalled();
   });
 });
