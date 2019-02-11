ea8d51018cc2ca30ff5d1c6387a12753d3d78e6c
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2019-02-11 18:29:27 -0200

    [ADD] Criacao de testes para o service TicTacToeResult

--- src/app/services/tic-tac-toe-result.service.spec.ts
+++ src/app/services/tic-tac-toe-result.service.spec.ts
@@ -13,4 +13,49 @@ describe('Service: TicTacToeResult', () => {
   it('should ...', inject([TicTacToeResultService], (service: TicTacToeResultService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get winner result - row combination', inject([TicTacToeResultService], (service: TicTacToeResultService) => {
+    const gameBoard = new Map<string, string>();
+    gameBoard.set('00', 'X');
+    gameBoard.set('01', 'X');
+    gameBoard.set('02', 'X');
+    gameBoard.set('11', 'O');
+    gameBoard.set('21', 'O');
+    expect(service.isWinner('X', gameBoard)).toBeTruthy();
+  }));
+
+  it('should get winner result - column combination', inject([TicTacToeResultService], (service: TicTacToeResultService) => {
+    const gameBoard = new Map<string, string>();
+    gameBoard.set('00', 'X');
+    gameBoard.set('10', 'X');
+    gameBoard.set('20', 'X');
+    gameBoard.set('11', 'O');
+    gameBoard.set('21', 'O');
+    expect(service.isWinner('X', gameBoard)).toBeTruthy();
+  }));
+
+  it('should get winner result - diagonal combination', inject([TicTacToeResultService], (service: TicTacToeResultService) => {
+    const gameBoard = new Map<string, string>();
+    gameBoard.set('00', 'X');
+    gameBoard.set('11', 'X');
+    gameBoard.set('22', 'X');
+    gameBoard.set('12', 'O');
+    gameBoard.set('21', 'O');
+    expect(service.isWinner('X', gameBoard)).toBeTruthy();
+  }));
+
+  it('should not get winner result and get draw result', inject([TicTacToeResultService], (service: TicTacToeResultService) => {
+    const gameBoard = new Map<string, string>();
+    gameBoard.set('00', 'X');
+    gameBoard.set('01', '0');
+    gameBoard.set('02', 'X');
+    gameBoard.set('10', 'O');
+    gameBoard.set('11', 'O');
+    gameBoard.set('12', 'X');
+    gameBoard.set('20', 'X');
+    gameBoard.set('21', 'X');
+    gameBoard.set('22', 'O');
+    expect(service.isWinner('X', gameBoard)).toBeFalsy();
+    expect(service.isDraw(gameBoard)).toBeTruthy();
+  }));
 });
