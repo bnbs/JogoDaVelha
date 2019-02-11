0a772006791871cfee9742ee9c01641a31dbb01d
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2019-02-11 17:06:25 -0200

    [ADD] Criaçao de testes para o component TicTacToe

--- src/app/components/tic-tac-toe/tic-tac-toe.component.spec.ts
+++ src/app/components/tic-tac-toe/tic-tac-toe.component.spec.ts
@@ -1,17 +1,23 @@
 /* tslint:disable:no-unused-variable */
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { AlertService } from 'src/app/services/alert.service';
+import { TicTacToeService } from 'src/app/services/tic-tac-toe.service';
+import { GameBoardComponent } from '../game-board/game-board.component';
+import { GamePanelComponent } from '../game-panel/game-panel.component';
+import { SelectHeroComponent } from '../select-hero/select-hero.component';
 import { TicTacToeComponent } from './tic-tac-toe.component';
 
 describe('TicTacToeComponent', () => {
   let component: TicTacToeComponent;
   let fixture: ComponentFixture<TicTacToeComponent>;
+  let debugElement: DebugElement;
+  let ticTacToeService: TicTacToeService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TicTacToeComponent ],
+      declarations: [ TicTacToeComponent, SelectHeroComponent, GamePanelComponent, GameBoardComponent ],
+      providers: [ TicTacToeService, AlertService ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
@@ -20,10 +26,41 @@ describe('TicTacToeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TicTacToeComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
+    ticTacToeService = debugElement.injector.get(TicTacToeService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should startGame and loadGame be false', () => {
+    expect(component.startGame).toBeFalsy();
+    expect(component.loadGame).toBeFalsy();
+  });
+
+  it('should loadGame be true', () => {
+    component.onLoadGame();
+    expect(component.loadGame).toBeTruthy();
+  });
+
+  it('should startGame be true', () => {
+    component.selectedHeroes([{name: 'Hulk', thumbnail: ''}, {name: 'Groot', thumbnail: ''}]);
+    expect(component.startGame).toBeTruthy();
+  });
+
+  it('should firstToPlay be 2 and initializeGame method to be called', () => {
+    spyOn(ticTacToeService, 'getFirstToPlay').and.returnValue(2);
+    const initializeGame = spyOn(ticTacToeService, 'initializeGame').and.returnValue(2);
+    component.startNewGame();
+    expect(component.firstToPlay).toBe(2);
+    expect(initializeGame).toHaveBeenCalled();
+  });
+
 });
