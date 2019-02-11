52128e6467850f7694f67b5dd4cd85b434516fd5
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2019-02-11 11:30:53 -0200

    [ADD] Criaçao teste para verificar o placar do jogador [ADD] Criacao teste para verificar texto que mostra qual player, se 1 ou 2

--- src/app/components/game-panel/scoreboard/scoreboard.component.spec.ts
+++ src/app/components/game-panel/scoreboard/scoreboard.component.spec.ts
@@ -4,25 +4,42 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { ScoreboardComponent } from './scoreboard.component';
+import { TicTacToeService } from 'src/app/services/tic-tac-toe.service';
+import { of } from 'rxjs';
 
 describe('ScoreboardComponent', () => {
   let component: ScoreboardComponent;
   let fixture: ComponentFixture<ScoreboardComponent>;
+  let debugElement: DebugElement;
+  let ticTacToeService: TicTacToeService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScoreboardComponent ]
+      declarations: [ ScoreboardComponent ],
+      providers: [ TicTacToeService ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ScoreboardComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
+    ticTacToeService = debugElement.injector.get(TicTacToeService);
+    spyOn(ticTacToeService, 'getScore').and.callThrough().and.returnValue(of([2, 1]));
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show Player 1 message', () => {
+    const message = debugElement.query(By.css('.player')).nativeElement.innerText;
+    expect(message).toBe('Player 1');
+  });
+
+  it('should show user score 2 ', () => {
+    const message = debugElement.query(By.css('h2')).nativeElement.innerText;
+    expect(message).toBe('2');
   });
 });
