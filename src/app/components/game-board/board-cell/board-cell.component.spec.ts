1ba3e23767026d6384555dd4f03419543b9c5a1c
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2019-02-11 13:49:08 -0200

    [ADD] Criaçao de testes para o component BoardCell

--- src/app/components/game-board/board-cell/board-cell.component.spec.ts
+++ src/app/components/game-board/board-cell/board-cell.component.spec.ts
@@ -8,6 +8,7 @@ import { BoardCellComponent } from './board-cell.component';
 describe('BoardCellComponent', () => {
   let component: BoardCellComponent;
   let fixture: ComponentFixture<BoardCellComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,10 +20,39 @@ describe('BoardCellComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BoardCellComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    component.cellPosition = '00';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show X', () => {
+    component.cellMark = 'X';
+    setTimeout(() => {
+      const marker = debugElement.query(By.css('span')).nativeElement.innerText;
+      expect(marker).toBe('X');
+    }, 1000);
+  });
+
+  it('should show O', () => {
+    component.cellMark = 'O';
+    setTimeout(() => {
+      const marker = debugElement.query(By.css('span')).nativeElement.innerText;
+      expect(marker).toBe('O');
+    }, 1000);
+  });
+
+  it('should have border-top-0 and border-left-0 class', () => {
+      const marker = debugElement.query(By.css('div.border-top-0.border-left-0')).nativeElement;
+      expect(marker).toBeDefined();
+  });
+
+  it('should not have border-bottom-0 and border-right-0 class', () => {
+    const marker = debugElement.query(By.css('div.border-bottom-0.border-right-0'));
+    expect(marker).toBeNull();
+  });
+
 });
